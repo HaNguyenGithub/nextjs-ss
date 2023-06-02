4fefc7015a75102afbca9a4b52bf430e18b7d78f
HaNguyenGithub <270297vh@gmail.com>
2023-06-02 10:37:21 +0700

    06/02/2023

--- src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-//Đây là file chứa toàn bộ các pages
-//Để cho cả navbar và footer xuất hiện thì dùng layout rồi khai báo trong file này
-import Layout from '@/components/Layout'
-import '@/styles/globals.css' //->file css cho index
-
-export default function App({ Component, pageProps }) {
-  return (<Layout><Component {...pageProps} /></Layout>)
-}
