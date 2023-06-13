a68d0614f96d8f1a0ec42d82cb001759f93d04c1
HaNguyenGithub <270297vh@gmail.com>
2023-06-13 10:59:59 +0700

    css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  purge: [
+    "./src/**/*.{html,js,svelte,ts,css,module.css}"
+    // "./src/**/*.html",
+    // "./src/**/*.vue,
+    // "./src/**/*.jsx"
+   ],
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,svelte,ts,css}',
-    './src/styles/record.{html,js,svelte,ts,css}'
+    './src/**/*.{html,js,svelte,ts,css,module.css}',
+    './src/styles/record.{html,js,svelte,ts,css,module.css}'
   ],
   theme: {
     extend: {},
